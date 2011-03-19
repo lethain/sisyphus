c129d03d56dfd9b137050093886b098c428aeadd
Will Larson <lethain@gmail.com>
2011-03-19 10:56:24 -0700

    Initial commit.

---
+++ views.py
@@ -0,0 +1,7 @@
+"Views for blog application."
+from django.shortcuts import render_to_response
+
+def frontpage(request):
+    "Render frontpage."
+    
+    return render_to_response('blog/front.html', {})
