83dda145ddf3c86cf4e995e3e60751c2bea699eb
Will Larson <lethain@gmail.com>
2011-03-24 23:26:39 -0700

    Add update_page management command

--- TODO.markdown
+++ TODO.markdown
@@ -1,15 +1,15 @@
-* articles to write backwards compat
-  * /articles/
-  * /coding-projects/
-  * /will-larson/
 * we have ``python ../local-sisyphus/blog/manage.py  update_page ../blog/example.html``
     for deploying stories, but need the ecosystem for generating the clean HTML I want...
 * add facebook metadata
 * add social sharing
 * 500 and 404 pages
-* deploy
-    * turn off debug
+
+* articles to write backwards compat
+  * /articles/
+  * /coding-projects/
+  * /will-larson/
 * write article on blog explaining stuff
+    * v3
     * trending
     * nginx rewrites for url backwards compatibility
     * search
@@ -17,9 +17,12 @@
     * similar content
     * more wiki-like
     * analytics
+* deploy
+    * turn off debug
 
 V2:
 
+* import comments from irrational exuberance v2.0
 * analytics
   * implement analytics module
   * implement analytics
---
+++ management/commands/update_page.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand, CommandError
+import sisyphus.models
+import json
+import time
+
+class Command(BaseCommand):
+    args = "<file_to_load file_to_load ...>"
+    help = "Load or update pages."
+    
+    def handle(self, *args, **options):
+        for file in args:
+            with open(file, 'r') as fin:
+                meta = ["{"]
+                html = []        
+                ended = False
+                for line in fin.readlines():
+                    if not ended and len(line.strip()) == 0:
+                        ended = True
+                    elif ended:
+                        html.append(line.strip())
+                    else:
+                        meta.append(line)
+                meta.append("}")
+                page = json.loads(u"\n".join(meta))
+                if 'pub_date' not in page:
+                    page['pub_date'] = int(time.time())
+                if 'tags' not in page:
+                    page['tags'] = []
+
+                page['html'] = u"\n".join(html)
+                sisyphus.models.add_page(page)
+
+
