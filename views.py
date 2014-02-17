6efeb9c7301e8d018124045dd1b205d63003372e
Will Larson <lethain@gmail.com>
2014-02-17 13:03:43 -0800

    Update author image, desc.

--- views.py
+++ views.py
@@ -52,8 +52,8 @@ def generate_feed(request, page_dicts, cli):
 def about_module(cli=None):
     "An 'About Me' module."
     html = """<img src="/static/author.png">
-<p>Eng leader at <a href="http://socialcode.com/">SocialCode</a>.<br>
-Used to <a href="http://digg.com/">Digg</a>, and <a href="http://developer.yahoo.com/search/boss/">Y!</a>.<br>
+<p>Eng leader at <a href="http://socialcode.com/">SocialCode</a><br>
+Used to <a href="http://digg.com/">Digg</a>, and <a href="http://developer.yahoo.com/search/boss/">Y!</a><br>
 Email: lethain[at]gmail</p>"""
     return { 'title':'Will Larson', 'html':html }
 
